c1b4eba2aad2ec36de0bdcce9e26738c6e82a364
Saulo Lordão <saulo.lordao@dcomp.ufs.br>
2013-09-06 14:21:40 -0300

    Primeiro commit

--- Simulador/src/projeto/processador/componentes/Barramento.java
+++ Simulador/src/projeto/processador/componentes/Barramento.java
@@ -1,45 +1,45 @@
 package projeto.processador.componentes;
 
 public class Barramento {
-	private static final Barramento dados = new Barramento(TipoBarramento.Dados);
-	private static final Barramento endereco = new Barramento(TipoBarramento.Endereco);
-	private static final Barramento controle = new Barramento(TipoBarramento.Controle);
-	
-	private TipoBarramento tipo;
-	private int sinal;
-	
-	private Barramento(TipoBarramento tipo) {
-		this.tipo = tipo;
-	}
-	
-	public static Barramento getBarramentoControle() {
-		return controle;
-	}
-	
-	public static Barramento getBarramentoDados() {
-		return dados;
-	}
-	
-	public static Barramento getBarramentoEndereco() {
-		return endereco;
-	}
-	
-	public void escrever(int palavra) {
-		switch (tipo) {
-		case Controle:
-			this.sinal = palavra & 1;
-			break;
-		default:
-			this.sinal = palavra % Memoria.TAMANHO_PALAVRA;
-			break;
-		}
-	}
-	
-	public int ler() {
-		return sinal;
-	}
-	
-	private enum TipoBarramento {
-		Dados, Endereco, Controle
-	}
+    private static final Barramento dados    = new Barramento(TipoBarramento.Dados);
+    private static final Barramento endereco = new Barramento(TipoBarramento.Endereco);
+    private static final Barramento controle = new Barramento(TipoBarramento.Controle);
+
+    private TipoBarramento tipo;
+    private int sinal;
+
+    private Barramento(TipoBarramento tipo) {
+        this.tipo = tipo;
+    }
+
+    public static Barramento getBarramentoControle() {
+        return controle;
+    }
+
+    public static Barramento getBarramentoDados() {
+        return dados;
+    }
+
+    public static Barramento getBarramentoEndereco() {
+        return endereco;
+    }
+
+    public void escrever(int palavra) {
+        switch (tipo) {
+        case Controle:
+            this.sinal = palavra & 1;
+            break;
+        default:
+            this.sinal = palavra % Memoria.TAMANHO_PALAVRA;
+            break;
+        }
+    }
+
+    public int ler() {
+        return sinal;
+    }
+
+    private enum TipoBarramento {
+        Dados, Endereco, Controle
+    }
 }
--- Simulador/src/projeto/processador/componentes/Memoria.java
+++ Simulador/src/projeto/processador/componentes/Memoria.java
@@ -1,17 +1,22 @@
 package projeto.processador.componentes;
 
 public class Memoria {
-	private static final Memoria instance = new Memoria();
-	public static final int TAMANHO_PALAVRA = (int) Math.pow(2, 16);
-	public static final int TAMANHO_MEMORIA = (int) Math.pow(2, 12);
-	
-	private int[] mem;
-	
-	private Memoria() {
-		mem = new int[TAMANHO_MEMORIA];
-	}
-	
-	public static Memoria getInstance() {
-		return instance;
-	}
+    private static final Memoria instance = new Memoria();
+    public static final int TAMANHO_PALAVRA = (int) Math.pow(2, 16);
+    public static final int TAMANHO_MEMORIA = (int) Math.pow(2, 12);
+
+    private int[] mem;
+
+    private Memoria() {
+        mem = new int[TAMANHO_MEMORIA];
+    }
+
+    public static Memoria getInstance() {
+        return instance;
+    }
+
+    public void acesso() {
+        int idx = Barramento.getBarramentoEndereco().ler();
+        Barramento.getBarramentoDados().escrever(mem[idx]);
+    }
 }
