46d3eb8d471eb23915834d04de2a183f8663172c
Saulo Lordão <saulo.lordao@dcomp.ufs.br>
2013-09-20 16:43:18 -0300

    Pequenas alterações

--- Simulador/src/projeto/computador/processador/Processador.java
+++ Simulador/src/projeto/computador/processador/Processador.java
@@ -1,15 +1,70 @@
 package projeto.computador.processador;
 
+import projeto.computador.Barramento;
+
 public class Processador {
+	private Ula ula;
+	private Registrador pc;
+	private Registrador ir;
+	private Registrador mar;
+	private Registrador mbr;
+
+	private Registrador ax;
+	private Registrador bx;
+	private Registrador cx;
+	private Registrador eax;
+	private Registrador ebx;
+
 	private static Processador instance;
-	
+
 	public static Processador getInstance() {
 		if (instance == null) {
 			instance = new Processador();
 		}
 		return instance;
 	}
-	
+
 	private Processador() {
+		ula = new Ula();
+
+		ax = new Registrador("AX");
+		bx = new Registrador("BX");
+		cx = new Registrador("CX");
+		eax = new Registrador("EAX");
+		ebx = new Registrador("EBX");
+	}
+
+	public void buscarInstrucao() {
+		Barramento bEnd      = Barramento.getBarramentoEndereco();
+		Barramento bControle = Barramento.getBarramentoControle();
+		
+		bEnd.escrever(pc.getPalavra());
+		bControle.escrever(1);
+	}
+	
+	public void lerInstrucao() {
+		Barramento bDados = Barramento.getBarramentoDados();
+		
+		ir.setPalavra(bDados.ler());
+	}
+
+	public int getAx() {
+		return ax.getPalavra();
+	}
+
+	public int getBx() {
+		return bx.getPalavra();
+	}
+
+	public int getCx() {
+		return cx.getPalavra();
+	}
+
+	public int getEax() {
+		return eax.getPalavra();
+	}
+
+	public int getEbx() {
+		return ebx.getPalavra();
 	}
 }
--- Simulador/src/projeto/computador/processador/Ula.java
+++ Simulador/src/projeto/computador/processador/Ula.java
@@ -1,40 +1,42 @@
 package projeto.computador.processador;
 
-class Ula {
-	private final static Ula instance = new Ula();
-	
-	private short oprd1;
-	private short oprd2;
+class Ula {	
+	private int oprd1;
+	private int oprd2;
 	private Registrador regUla;
 	
-	private short opcode;
+	private int opcode;
 	
-	private Ula() {
+	Ula() {
 		regUla = new Registrador("Registrador de Estado (ULA)");
 	}
 	
-	static Ula getInstance() {
-		return instance;
-	}
-	
-	void setOprd1(short oprd) {
+	void setOprd1(int oprd) {
 		this.oprd1 = oprd;
 	}
 	
-	void setOprd2(short oprd) {
+	void setOprd2(int oprd) {
 		this.oprd2 = oprd;
 	}
 	
-	short executar() {
+	int executar() {
 		int result = 0;
 		switch (opcode) {
 		
 		}
 		
-		short zero     = (short) (1 & result),
-			  negativo = (short) (result >>> 31),
-			  overflow;
+		int zero     = 1 & result,
+			negativo = result >>> 31,
+			overflow = result >>> 15 != 0 ? 1 : 0;
 		
-		return (short) result;
+		int palavra = overflow;
+		palavra = palavra << 1;
+		palavra = palavra | negativo;
+		palavra = palavra << 1;
+		palavra = palavra | zero;
+		
+		regUla.setPalavra(palavra);
+		
+		return result;
 	}
 }
